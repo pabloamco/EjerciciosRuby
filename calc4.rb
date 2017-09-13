07b01abf69fc38f30e8d2f7a0366b97e2e0c2929
Pablo Leon <pablol@admins-MacBook-Pro.local>
2017-09-13 16:17:56 -0500

    Jefe enojado

---
+++ calc4.rb
@@ -0,0 +1,6 @@
+gets
+puts 'Que quieres suarez? que no sabe que estoy ocupado?'
+gets.chomp
+puts 'De que demonios hablas Suarez!! largo de mi offi alv!! xD'
+gets.chomp
+puts 'Despedido dije!! LARGO'
