9eb37a0b48b30ac3cb78fb4237fd4757830207a2
Pablo Leon <pablol@admins-MacBook-Pro.local>
2017-09-15 11:10:54 -0500

    99 botellas de cerveza

---
+++ calc5.rb
@@ -0,0 +1,12 @@
+botellas = 99
+	while botellas >= 1
+	 puts "#{botellas} botellas de cerveza en la pared, #{botellas}  botellas de cerveza"
+
+botellas = botellas - 1
+	 if botellas == 0
+	  puts "no hay mas botellas de cerveza"
+	 else
+	  puts "Toma una y pasala al rededor #{botellas} botellas de cerveza en la pared"
+	  puts "Si una de las botellas se llegara a caer #{botellas} botelas de cerveza en la pared"
+	 end
+	end
